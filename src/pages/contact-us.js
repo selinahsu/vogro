81b4c9a9ec498b79f445d05136ea827fba0e032f
selinahsu <selinaraindrop@gmail.com>
2020-09-13 14:14:23 -0400

    rm earlier statelss form setup

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -61,112 +61,108 @@ class ContactUs extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.form} name="contact" method="POST" data-netlify="true">
-        <Container maxWidth="md">
-          <Typography variant="h4" color="black" gutterBottom>
-            Contact Us
-          </Typography>
-          <Typography variant="body1" color="black" gutterBottom>
-          Having issues with our service? Want to reach out? Enter in your message and we'll get back to you shortly.
-          </Typography>
+      <Container maxWidth="md">
+        <Typography variant="h4" color="black" gutterBottom>
+          Contact Us
+        </Typography>
+        <Typography variant="body1" color="black" gutterBottom>
+        Having issues with our service? Want to reach out? Enter in your message and we'll get back to you shortly.
+        </Typography>
 
-          <form className={classes.form} onSubmit={this.handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="firstName"
-                  inputProps={{ 'name': 'firstName' }}
-                  autoComplete="fname"
-                  variant="outlined"
-                  fullWidth
-                  label="First Name"
-                  value={ this.state.firstName }
-                  onChange={(event) => {
-                    this.setState({ firstName: event.target.value })
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="lastName"
-                  inputProps={{ 'name': 'lastName' }}
-                  autoComplete="lname"
-                  variant="outlined"
-                  fullWidth
-                  label="Last Name"
-                  value={ this.state.lastName }
-                  onChange={(event) => {
-                    this.setState({ lastName: event.target.value })
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  variant="outlined"
-                  fullWidth
-                  label="Email"
-                  value={ this.state.email }
-                  onChange={(event) => {
-                    this.setState({ email: event.target.value })
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Select
-                  name="msgType"
-                  variant="outlined"
-                  fullWidth
-                  displayEmpty
-                  inputProps={{ 'aria-label': 'Without label' }}
-                  open={this.state.open}
-                  onClose={this.handleClose}
-                  onOpen={this.handleOpen}
-                  value={ this.state.msgType }
-                  onChange={(event) => {
-                    this.setState({ msgType: event.target.value })
-                  }}
-                >
-                  <MenuItem value="" disabled>
-                    <em>Please select a message type</em>
-                  </MenuItem>
-                  <MenuItem value={'Helper'}>Helper</MenuItem>
-                  <MenuItem value={'Someone in Need'}>Someone in Need</MenuItem>
-                  <MenuItem value={'General Question'}>General Question</MenuItem>
-                </Select>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="message"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  label="Message"
-                  inputProps={this.inputProps}
-                  value={ this.state.message }
-                  onChange={(event) => {
-                    this.setState({ message: event.target.value })
-                  }}
-                />
-              </Grid>
+        <form className={classes.form} onSubmit={this.handleSubmit} name="contact">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                autoComplete="fname"
+                variant="outlined"
+                fullWidth
+                label="First Name"
+                value={ this.state.firstName }
+                onChange={(event) => {
+                  this.setState({ firstName: event.target.value })
+                }}
+              />
             </Grid>
-            <div class="field">
-              <div data-netlify-recaptcha="true"></div>
-            </div>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              disableElevation
-              className={classes.submit}
-            >
-              Submit
-            </Button>
-          </form>
-        </Container>
-      </div>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="lastName"
+                autoComplete="lname"
+                variant="outlined"
+                fullWidth
+                label="Last Name"
+                value={ this.state.lastName }
+                onChange={(event) => {
+                  this.setState({ lastName: event.target.value })
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="email"
+                type="email"
+                autoComplete="email"
+                variant="outlined"
+                fullWidth
+                label="Email"
+                value={ this.state.email }
+                onChange={(event) => {
+                  this.setState({ email: event.target.value })
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Select
+                name="msgType"
+                variant="outlined"
+                fullWidth
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                open={this.state.open}
+                onClose={this.handleClose}
+                onOpen={this.handleOpen}
+                value={ this.state.msgType }
+                onChange={(event) => {
+                  this.setState({ msgType: event.target.value })
+                }}
+              >
+                <MenuItem value="" disabled>
+                  <em>Please select a message type</em>
+                </MenuItem>
+                <MenuItem value={'Helper'}>Helper</MenuItem>
+                <MenuItem value={'Someone in Need'}>Someone in Need</MenuItem>
+                <MenuItem value={'General Question'}>General Question</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="message"
+                variant="outlined"
+                fullWidth
+                multiline
+                label="Message"
+                inputProps={this.inputProps}
+                value={ this.state.message }
+                onChange={(event) => {
+                  this.setState({ message: event.target.value })
+                }}
+              />
+            </Grid>
+          </Grid>
+          <div class="field">
+            <div data-netlify-recaptcha="true"></div>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disableElevation
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </form>
+      </Container>
     );
   }
 }
